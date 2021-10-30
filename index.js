6605069fbf32eeab16e9f453e98d93518b8fba68
chandan9074 <chandan15-11611@diu.edu.bd>
2021-10-30 23:53:13 +0600

    feat: create usefull api

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 require('dotenv').config()
 const cors = require("cors");
+const ObjectId = require('mongodb').ObjectId;
 
 
 app.use(cors());
@@ -24,6 +25,7 @@ async function run() {
 
     const database = client.db("wildWishesdb");
     const serviceCollection = database.collection("services");
+    const bookingsCollection = database.collection("bookings");
 
 
     // get api
@@ -32,7 +34,51 @@ async function run() {
         const cursor = serviceCollection.find({});
         const services = await cursor.toArray();
         res.send(services)
+        // console.log(services)
         
+    })
+
+    app.get("/my-bookings", async (req, res)=>{
+
+        const cursor = bookingsCollection.find({});
+        const services = await cursor.toArray();
+        res.send(services)
+        // console.log(services)
+        
+    })
+
+    app.get("/services/:id", async (req, res)=>{
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const user = await serviceCollection.findOne(query)
+      // console.log(id)
+      res.send(user);
+    })
+
+     // post api
+    app.post('/bookings', async (req, res)=>{
+      const data = req.body;
+      const result = await bookingsCollection.insertOne(data);
+      console.log("here krishna", result);
+      res.json(result);
+    })
+
+     // post api
+    app.post('/services', async (req, res)=>{
+      const data = req.body;
+      const result = await serviceCollection.insertOne(data)
+      // console.log("here krishna", result);
+      res.json(result)
+    })
+
+    //delete api
+    app.delete('/bookings/:id', async (req, res)=>{
+      const userid = req.params.id;
+      const query = {_id: ObjectId(userid)};
+      const result = await bookingsCollection.deleteOne(query);
+
+      res.json(result);
+
     })
     
 
