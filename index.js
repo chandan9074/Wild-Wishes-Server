084f46e50866a5e0b5a2b8b23a81355437321f30
chandan9074 <chandan15-11611@diu.edu.bd>
2021-10-31 20:40:24 +0600

    refact: add port

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const ObjectId = require('mongodb').ObjectId;
 app.use(cors());
 app.use(express.json());
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.z2qxz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -93,6 +93,9 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res)=>{
+    res.send("Responding server")
+})
 
 app.listen(port, ()=>{
     console.log(`lisiting port on ${port}`)
